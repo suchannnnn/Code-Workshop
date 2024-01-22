65b5d33880f8cbaa4c618b083b0c0027aedf4ea9
chan <157358749+suchannnnn@users.noreply.github.com>
2024-01-22 17:37:47 +0800

    Add files via upload

---
+++ celsius_to_fahrenheit_converter.py
@@ -0,0 +1,12 @@
+# print conversion title
+print("2. Conversion from Celsius to Fahrenheit")
+# input value for celsius
+a=input("Enter a value for Celsius: ")
+# process of converting values with the use of formula for celsius to fahrenheit
+b=(float(a)*9/5)+32
+# displaying converted values
+print(a, "Celsius is equals to", b, "in Fahrenheit")
+
+# Christian R. Sumalinog
+# PS99
+# 11/23/2023
---
+++ fahrenheit_to_celsius_converter.py
@@ -0,0 +1,12 @@
+# print conversion title
+print("1. Conversion from Fahrenheit to Celsius")
+# input value for fahrenheit
+a=input("Enter a value for Fahrenheit: ")
+# process of converting values with the use of formula for fahrenheit to celsius
+b=(float(a)-32)*5/9
+# displaying converted values
+print(a, "Fahrenheit is equals to", b, "in Celsius")
+
+# Christian R. Sumalinog
+# PS99
+# 11/23/2023
